52b01a6f75c1983188f8c4c2e26a9a1f6e1cae8d
nodbew <165175929+nodbew@users.noreply.github.com>
2024-12-02 19:08:28 +0900

    Implement logic

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ submitButton.addEventListener('click', () => {
   answerArea.style.display = 'none';
   resultArea.style.display = 'block';
   // To add stylings for mistaken parts
-  resultArea.innerHTML = correctMistakes(answerArea.value);
+  resultArea.appendChild(correctMistakes(split(answerArea.value), settingArea.dataset.answer, settingArea.dataset.problem));
 });
 proceedButton.addEventListener('click', () => {
   submitButton.classList.add('active');
@@ -31,20 +31,43 @@ proceedButton.addEventListener('click', () => {
     answerArea.value = generateProblem(settingArea.dataset.problem, deletionSetting.dataset.deletionNum);
   }
 });
-settingArea.addEventListener('input', (e) => { settingArea.dataset.problem = e.target.value.split(' ') });
+settingArea.addEventListener('input', (e) => { settingArea.dataset.problem = split(e.target.value) });
 deletionSetting.addEventListener('change', (e) => { deletionSetting.dataset.deletionNum = e.target.value });
 
-function correctMistakes(userAnswer) {
-  return userAnswer;
+function correctMistakes(userAnswer, correctAnswers, problem) {
+  for (idx in correctAnswers) {
+    if (userAnswer[idx] !== problem[idx]) {
+      const span = document.createElement('span');
+      span.style.fontColor = 'red';
+      span.textContent = userAnswer[idx];
+      userAnswer[idx] = span;
+    }
+  }
+
+  const container = document.createElement('p');
+  userAnswer.forEach(e => typeof e === 'string' ? container.textContent += e : container.appendChild(e));
+  return container;
 }
 
 function generateProblem(problem, num) {
-  return removeRandom([...problem]);
+  const prob = [...problem];
+  const answers = removeRandom(prob);
+  settingArea.dataset.answer = answers;
+  return prob.join(' ');
+}
 
 function removeRandom(options, numRemove) {
-  return [...new Array(numRemove)].forEach(_ => options.splice(getRandomNum(options.length), 1));
+  return [...new Array(numRemove)].map(_ => {
+    const idx = getRandomNum(options.length);
+    options.splice(idx, 1);
+    return idx;
+  });
 }
   
 function getRandomNum(max) {
   return Math.floor(Math.random() * max);
+}
+
+function split(s) {
+  return s.trim().split(/[\s]/).filter(s => s !== '');
 }
